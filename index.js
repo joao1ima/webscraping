69377ffdc4a8a77e0d8f4d9c3eb324f79b3dc9f8
joao1ima <j.airon@hotmail.com>
2021-05-26 23:19:28 -0300

    ajustes nas funções e variáveis para consolidar a extração dos dados

--- index.js
+++ index.js
@@ -4,13 +4,37 @@ const cheerio = require('cheerio');
 
 const fatherUrl = 'https://www.gov.br/pt-br/noticias/ultimas-noticias'
 
-axios.get(fatherUrl)
-    .then(resp =>{
+const links = axios.get(fatherUrl)
+    .then(resp => {
         let htmlData = resp.data;
-        let Alldata = [];
+        let allData = [];
         let $ = cheerio.load(htmlData);
-        $('a[class="summary url"]').each((i,e)=>{
+        $('a[class="summary url"]').each((i, e) => {
             let summaryLink = $(e).attr('href');
-            
+            allData.push(summaryLink);
         })
+        return allData;
     })
+
+
+function extractData(link) {
+    axios.get(link)
+        .then(resp => {
+            let htmlAll = resp.data;
+            let $ = cheerio.load(htmlAll);
+            let articleTitle = $('h1').text();
+            let artileLead = $('div[class="documentDescription"]').text();
+            let articleDate = $('span[class="value"]').text();
+            console.log({ articleTitle, artileLead, articleDate })
+        })
+}
+
+
+async function main() {
+    const unitUrl = await links
+    unitUrl.map(i => {
+        extractData(i)
+    })
+}
+
+main()
